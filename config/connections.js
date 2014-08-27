8ebd4a7e4bcf1ba175e6cff6d44de50cba5030f6
Marcelo Boeira <marceloboeira@live.com>
2014-08-27 09:59:16 -0300

    New Deploy structure

--- config/connections.js
+++ config/connections.js
@@ -13,12 +13,12 @@ module.exports.connections = {
   stage: {
     adapter: process.env.STAGE_DB_ADAPTER ||  'sails-disk',
     url: process.env.STAGE_DB_URL || 'localhost',
-    ssl: true
+    ssl: process.env.STAGE_DB_SSL || true, 
   },
 
   production: {
     adapter: process.env.PROD_DB_ADAPTER ||  'sails-disk',
     url: process.env.PROD_DB_URL || 'localhost',
-    ssl: true
+    ssl: process.env.PROD_DB_SSL || true,
   }
 };
