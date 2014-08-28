f682b0478d6538456edf5e41741b8a4171b51113
Marcelo Boeira <marceloboeira@live.com>
2014-08-27 22:46:45 -0300

    #14 - Moment.js implemented...

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,5 +1,18 @@
 
 $(function(){
 	
-	
+	/*! 
+	  * Moment.js to make timestamps pretty 
+	  * 	
+	  *	@see https://github.com/vimia/blew/issues/14
+	  */
+	var momentLiveUpdate = function() {
+		var now = new Date();
+		$('date, time').each(function(i, e) {
+    		var d = moment($(e).attr('source'));
+			$(e).html(d.from(now));
+		});
+	};
+	momentLiveUpdate();
+	setInterval(momentLiveUpdate, 60000);
 });
