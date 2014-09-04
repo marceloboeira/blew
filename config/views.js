3b5b0a1f4e383e7d031d31ef658e82ae5526ff41
Marcelo Boeira <marceloboeira@live.com>
2014-09-04 11:18:48 -0300

    #37 - Injecting minification...

--- config/views.js
+++ config/views.js
@@ -1,5 +1,17 @@
+var minify = require('html-minifier').minify;
+var ejs = require('ejs');
+var parsing = function(path,options,fn) {
+	ejs.renderFile(path, options, function(err, str){
+  	str = minify(str,{collapseWhitespace: true});
+  	return fn(err, str);
+  	});
+  	
+};
 
 module.exports.views = {
-  engine: 'ejs',
+  engine: {
+  	ext: 'ejs',
+  	fn: parsing
+  },
   layout: 'layout/default'
 };
