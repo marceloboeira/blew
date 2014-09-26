cd8b05adef22fdc556ed49163bd734fa18273094
Marcelo Boeira <me@marceloboeira.com>
2014-09-26 13:21:34 -0300

    Finally HTML Minification its working, closes #37

--- config/views.js
+++ config/views.js
@@ -1,17 +1,36 @@
+
+
+/** 
+ * HTML Minification
+ * 	
+ * @see https://github.com/vimia/blew/issues/37
+ */
 var minify = require('html-minifier').minify;
-var ejs = require('ejs');
-var parsing = function(path,options,fn) {
-	ejs.renderFile(path, options, function(err, str){
-  	str = minify(str,{collapseWhitespace: true});
-  	return fn(err, str);
-  	});
-  	
+var ejs = require('ejs-locals');
+
+var parsing = function(path, options, cb) {
+    options.locals = options.locals || {};
+    
+    // Override the layout file
+    options.locals._layoutFile = '/layout/default.ejs';
+    ejs(path, options, function(err, str){
+    	// Minify   
+      str = minify(str,{collapseWhitespace: true, 
+      									removeComments: true,
+      									collapseBooleanAttributes: true,
+      									minifyJS: true,
+      									minifyCSS: true,
+      									minifyURLs: true
+      								});
+      return cb(err, str);
+    });
+
 };
 
 module.exports.views = {
   engine: {
-  	ext: 'ejs',
-  	fn: parsing
+    ext: 'ejs',
+    fn: parsing // <-- Function to parse the view
   },
-  layout: 'layout/default'
+  layout: false // <-- Disable layout
 };
