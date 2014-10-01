93835bd0938d9257242667266263732f358ce4a7
Marcelo Boeira <me@marceloboeira.com>
2014-10-01 18:33:11 -0300

    #3 - Model validations, parsing time with filter and isExpired, itExpires methods...

--- api/models/Paste.js
+++ api/models/Paste.js
@@ -33,21 +33,25 @@ module.exports = {
     	defaultsTo: null
     },
 
+    expiresAt:{
+      type:'date',
+      required:false,
+      defaultsTo: null
+    },
+
     /** 
      * Get the Paste Link, ...
      * 
      * @param full - With the 'http://blew.io/'
-     *
-     * @return 'http://blew.io/p/12b78c92x0136xzb0ks94jg90'
+     * @return 'http://blew.io/p/5422d5f8844e460b00ac36fb'
      */
-    getLink: function(full){
-      return ((full === true) ? sails.config.globals.baseUrl : "" ) + sails.config.routes.paste + this.id;
+    getLink: function(f){
+      return ((f === true) ? sails.config.globals.baseUrl : "" ) + sails.config.routes.paste + this.id;
     },
 
     /** 
      * Verify if paste is Private
      * 
-     *
      * @return Boolean
      */
     isPrivate: function() {
@@ -55,10 +59,29 @@ module.exports = {
     },
 
     /** 
+     * Verify if the paste expires
+     *
+     * @see https://github.com/vimia/blew/issues/3
+     * @return Boolean
+     */
+    itExpires: function() {
+      return (this.expiresAt !== null);
+    },
+
+    /** 
+     * Verify if the paste is expired
+     *
+     * @see https://github.com/vimia/blew/issues/3
+     * @return Boolean
+     */
+    isExpired: function() {
+      return (this.itExpires() ? (new Date().getTime() > new Date(this.expiresAt).getTime()) : false);
+    },
+
+    /** 
      * Verify if the given ID is the owner.
      * 
      * @param u - User Object or User Id
-     *
      * @return Boolean
      */
     isOwner: function(u) {
@@ -74,8 +97,17 @@ module.exports = {
 	
   },
 
+  beforeValidate: function(attrs, cb) {
+
+    // @see https://github.com/vimia/blew/issues/3
+    attrs.expiresAt = (attrs.expiresAt !== null) ? FilterService.durationToDate(attrs.expiresAt) : null;
+
+    return cb();
+  },
+
   beforeCreate: function(attrs, cb){
     attrs.language = detectLanguage(attrs.content) || attrs.language || 'Unknow';
+  
     return cb();
   }
 };
