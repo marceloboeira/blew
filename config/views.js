93aa4c293b3a5ecfd77e005e2a8ce879b66220c4
Marcelo Boeira <me@marceloboeira.com>
2014-12-25 16:18:48 -0200

    #77 - Rectoring ...

--- config/views.js
+++ config/views.js
@@ -7,17 +7,17 @@ var minify = require('html-minifier').minify;
 var ejs = require('ejs-locals');
 
 var parser = function(path, options, cb) {
-    options.locals = options.locals || {};
+  options.locals = options.locals || {};
     
-    // Override the layout file
-    options.locals._layoutFile = '/layouts/public.ejs';
-    ejs(path, options, function(err, str){
-      str = minify(str,{collapseWhitespace: true, conservativeCollapse: true, removeComments: true, collapseBooleanAttributes: true, minifyJS: true, minifyCSS: true, minifyURLs: true });
-      return cb(err, str);
-    });
-
+  // Override the layout file
+  options.locals._layoutFile = '/layouts/public.ejs';
+  ejs(path, options, function(err, str){
+    str = minify(str,{collapseWhitespace: true, conservativeCollapse: true, removeComments: true, collapseBooleanAttributes: true, minifyJS: true, minifyCSS: true, minifyURLs: true });
+    return cb(err, str);
+  });
 };
 
+/* Injecting the view-parser into the ejs engine */
 module.exports.views = {
   engine: {
     ext: 'ejs',
