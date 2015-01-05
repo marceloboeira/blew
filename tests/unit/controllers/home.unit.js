46a7d07bcbb4f228fefdf6ca9841eebb39b89045
Marcelo Boeira <me@marceloboeira.com>
2015-01-05 15:55:42 -0200

    Identing tests

--- tests/unit/controllers/home.unit.js
+++ tests/unit/controllers/home.unit.js
@@ -1,53 +1,41 @@
 var HomeController = require('../../../api/controllers/HomeController'),
-    sinon = require('sinon'),
-    assert = require('assert'),
-    Sails = require('sails'),
-    app;
+		sinon = require('sinon'),
+		assert = require('assert'),
+		Sails = require('sails'),
+		app;
 
-    before(function (done) {
-        this.timeout(5000);
-        Sails.lift({
-            log: {
-                level: 'error'
-            },
-            adapters: {
-                'default': 'sails-disk'  
-            }
+before(function (done) {
+	this.timeout(5000);
+	Sails.lift({log: {level: 'error'}, adapters: { 'default': 'sails-disk'}}, function (err, sails) {
+		app = sails;
+		done(err, sails);
+	});
+});
 
-        }, function (err, sails) {
-            app = sails;
-            done(err, sails);
-        });
-    });
-
-
-    after(function (done) {
-        app.lower(done);
-    });
+after(function (done) {
+	app.lower(done);
+});
 
 describe('The Home Controller', function () {
-    var view = sinon.spy();
-    var req = {};
-    var res = {view:view};
-    req.user = {user:{first_name:'oscar'}};
+	var view = sinon.spy();
+	var req = {};
+	var res = {view:view};
+	req.user = {user:{first_name:'oscar'}};
 
-    describe('when we load the home page', function () {
-
-        it ('should contain user', function () {
-            assert.ok(req.user);
-        });
-        it ('should render the view', function () {
-
-            HomeController.index(req,res, {
-                view: view,
-                title: 'Home - MEANS',
-                navItems: {},
-                currentUser: req.user,
-                locales: app.config.i18n.locales,
-                layout: '/layouts/internal'
-            });
-            assert.ok(view.called);
-        });
-
+	describe('when we load the home page', function () {
+		it ('should contain user', function () {
+			assert.ok(req.user);
     });
+    it ('should render the view', function () {
+			HomeController.index(req,res, {
+						view: view,
+						title: 'Home',
+						navItems: {},
+						currentUser: req.user,
+						locales: app.config.i18n.locales,
+						layout: '/layouts/internal'
+			});
+			assert.ok(view.called);
+		});
+	});
 });
