f3d557604f1044064dc8461c71527eb52f5a4369
Marcelo Boeira <me@marceloboeira.com>
2015-07-17 16:02:27 -0300

    feature: Add timestamps to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,22 @@
 class User
   include Mongoid::Document
-  
+  include Mongoid::Timestamps
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  field :email,              type: String, default: ""
-  field :encrypted_password, type: String, default: ""
-  field :reset_password_token,   type: String
+  field :email, type: String
+  field :encrypted_password, type: String
+  field :reset_password_token, type: String
   field :reset_password_sent_at, type: Time
   field :remember_created_at, type: Time
-  field :sign_in_count,      type: Integer, default: 0
+  field :sign_in_count, type: Integer, default: 0
   field :current_sign_in_at, type: Time
-  field :last_sign_in_at,    type: Time
+  field :last_sign_in_at, type: Time
   field :current_sign_in_ip, type: String
-  field :last_sign_in_ip,    type: String
-  field :confirmation_token,   type: String
-  field :confirmed_at,         type: Time
+  field :last_sign_in_ip, type: String
+  field :confirmation_token, type: String
+  field :confirmed_at, type: Time
   field :confirmation_sent_at, type: Time
-  field :unconfirmed_email,    type: String 
-
+  field :unconfirmed_email, type: String 
 end
